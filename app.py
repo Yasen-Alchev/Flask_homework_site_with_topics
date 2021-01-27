edbed023bb504711cc2ff8631952861076469eb1
Yasen-Alchev <yasen.m.alchev.2017@elsys-bg.org>
2021-01-27 23:27:04 +0200

    Working authentication added

--- app.py
+++ app.py
@@ -3,19 +3,21 @@ import os
 
 from flask import Flask
 from flask import render_template, request, redirect, make_response, url_for, flash
-from flask_login import login_user, login_required, current_user, logout_user
+from flask_login import login_user, login_required, current_user, logout_user, LoginManager
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from database import db_session, init_db
 from models import User, Post, Topic
+from login import login_manager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'CVEdiffiehelman'
+login_manager.init_app(app)
 init_db()
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template("error_404.html"), 400
+    return render_template("error_404.html"), 400
 
 @app.errorhandler(500)
 def internal_error(e):
@@ -27,27 +29,42 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
+            user.login_id = str(uuid.uuid4())
+            db_session.commit()
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        confirm_pasword = request.form["verify_password"]
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        secure_password = generate_password_hash(password)
+        confirm_pasword = request.form['verify_password']
         user = User.query.filter_by(username=username).first()
-        if(user is not None):
+        if user:
             flash("This username already exists!","danger")
             return render_template("register.html")
         if confirm_pasword == password:
-            user = User(username=username, password=generate_password_hash(password))
+            user = User(username=username, password=secure_password)
             db_session.add(user)
             db_session.commit()
-            flash("Registration complete!","success")
-            return redirect(url_for('login'))
+            return redirect(url_for('index'))
         else:
             flash("Passwords doesn`t match!","danger")
     return render_template("register.html")
 
+@app.route('/logout')
+@login_required
+def logout():
+    current_user.login_id = None
+    db_session.commit()
+    logout_user()
+    return redirect(url_for('login'))
